d52416c9e5cf3e4b33848f90d03e735c0f505019
Lucas Brémond <lucas.bremond@gmail.com>
2018-07-29 20:30:01 -0700

    [feature] Improve ITRF.test

--- test/Library/Physics/Coordinate/Frames/ITRF.test.cpp
+++ test/Library/Physics/Coordinate/Frames/ITRF.test.cpp
@@ -7,7 +7,9 @@
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+#include <Library/Physics/Environment/Objects/CelestialBodies/Earth.hpp>
 #include <Library/Physics/Coordinate/Frame.hpp>
+#include <Library/Physics/Units/Length.hpp>
 #include <Library/Physics/Units/Derived/Angle.hpp>
 
 #include <Library/Mathematics/Geometry/Transformations/Rotations/RotationMatrix.hpp>
@@ -17,18 +19,18 @@
 #include <Library/Core/FileSystem/Path.hpp>
 #include <Library/Core/Containers/Table.hpp>
 #include <Library/Core/Containers/Array.hpp>
-#include <Library/Core/Containers/Triple.hpp>
+#include <Library/Core/Containers/Tuple.hpp>
 #include <Library/Core/Types/Real.hpp>
 
 #include <Global.test.hpp>
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-TEST (Library_Physics_Coordinate_Frames_ITRF, Test_1)
+TEST (Library_Physics_Coordinate_Frames_ITRF, Test)
 {
 
     using library::core::types::Real ;
-    using library::core::ctnr::Triple ;
+    using library::core::ctnr::Tuple ;
     using library::core::ctnr::Array ;
     using library::core::ctnr::Table ;
     using library::core::fs::Path ;
@@ -40,18 +42,20 @@ TEST (Library_Physics_Coordinate_Frames_ITRF, Test_1)
     using library::math::geom::trf::rot::RotationVector ;
     using library::math::geom::trf::rot::RotationMatrix ;
     
+    using library::physics::units::Length ;
     using library::physics::units::Angle ;
     using library::physics::time::Scale ;
     using library::physics::time::Instant ;
     using library::physics::time::DateTime ;
     using library::physics::coord::Transform ;
     using library::physics::coord::Frame ;
+    using library::physics::env::obj::celest::Earth ;
 
     {
 
-        const Array<Triple<File, Angle, Real>> referenceScenarios =
+        const Array<Tuple<File, Angle, Length, Length, Real>> referenceScenarios =
         {
-            { File::Path(Path::Parse("../test/Library/Physics/Coordinate/Frames/ITRF/Test_1/Test_1 ITRF_GCRF.csv")), Angle::Arcseconds(1.0), 1e-12 }
+            { File::Path(Path::Parse("../test/Library/Physics/Coordinate/Frames/ITRF/Test_1/Test_1 ITRF_GCRF.csv")), Angle::Arcseconds(1.0), Length::Meters(0.1), Length::Meters(0.1), 1e-12 }
         } ;
 
         for (const auto& referenceScenario : referenceScenarios)
@@ -62,7 +66,7 @@ TEST (Library_Physics_Coordinate_Frames_ITRF, Test_1)
 
             // Reference data setup
 
-            const File referenceDataFile = referenceScenario.first ;
+            const File referenceDataFile = std::get<0>(referenceScenario) ;
 
             const Table referenceData = Table::Load(referenceDataFile, Table::Format::CSV, true) ;
 
@@ -89,12 +93,24 @@ TEST (Library_Physics_Coordinate_Frames_ITRF, Test_1)
                 EXPECT_EQ(q_GCRF_ITRF, q_ITRF_GCRF.toConjugate()) ;
                 EXPECT_EQ(w_GCRF_ITRF_in_GCRF, (q_ITRF_GCRF.toConjugate() * (-w_ITRF_GCRF_in_ITRF))) ;
 
-                const Angle toleranceAngle = referenceScenario.second ;
+                // Orientation
+
+                const Angle toleranceAngle = std::get<1>(referenceScenario) ;
 
                 EXPECT_GT(toleranceAngle.inDegrees(), RotationVector::Quaternion((q_GCRF_ITRF.toConjugate() / reference_q_ITRF_GCRF).rectify().normalize()).getAngle().inDegrees()) ;
                 EXPECT_GT(toleranceAngle.inDegrees(), RotationVector::Quaternion((q_ITRF_GCRF / reference_q_ITRF_GCRF).rectify().normalize()).getAngle().inDegrees()) ;
 
-                const Real toleranceAngularVelocity = referenceScenario.third ;
+                const Length positionalToleranceAtSurface = std::get<2>(referenceScenario) ;
+
+                EXPECT_GT(positionalToleranceAtSurface, Earth::EquatorialRadius * (q_GCRF_ITRF * Vector3d::X() - (reference_q_ITRF_GCRF.toConjugate() * Vector3d::X())).norm()) ;
+
+                const Length positionalToleranceAt1000km = std::get<3>(referenceScenario) ;
+
+                EXPECT_GT(positionalToleranceAt1000km, (Earth::EquatorialRadius + Length::Kilometers(1000.0)) * (q_GCRF_ITRF * Vector3d::X() - (reference_q_ITRF_GCRF.toConjugate() * Vector3d::X())).norm()) ;
+
+                // Check angular velocity
+
+                const Real toleranceAngularVelocity = std::get<4>(referenceScenario) ;
 
                 EXPECT_GT(toleranceAngularVelocity, (w_ITRF_GCRF_in_ITRF - reference_w_ITRF_GCRF_in_ITRF).norm()) ;
 
