2952bc47d8df1b87b2a47d00fd9374196701a548
Lucas Brémond <lucas.bremond@gmail.com>
2019-10-29 09:51:50 -0700

    [misc] Add tests

--- bindings/python/test/library/physics/test_environment.py
+++ bindings/python/test/library/physics/test_environment.py
@@ -9,12 +9,102 @@
 
 import pytest
 
+from Library.Physics.Time import Scale
+from Library.Physics.Time import Instant
+from Library.Physics.Time import DateTime
 from Library.Physics import Environment
+
+################################################################################################################################################################
+
+def test_environment_constructors ():
+
+    instant = Instant.DateTime(DateTime(2019, 1, 1, 0, 0, 0), Scale.UTC)
+    objects = []
+
+    assert Environment(instant, objects) is not None
+
+################################################################################################################################################################
+
+def test_environment_undefined ():
+
+    assert Environment.Undefined() is not None
 
 ################################################################################################################################################################
 
 def test_environment_default ():
 
     assert Environment.Default() is not None
+
+################################################################################################################################################################
+
+def test_environment_isDefined ():
+
+    environment = Environment.Default()
+
+    assert environment.isDefined() is not None
+
+################################################################################################################################################################
+
+def test_environment_hasObjectWithName ():
+
+    environment = Environment.Default()
+
+    assert environment.hasObjectWithName('Earth') is not None
+
+################################################################################################################################################################
+
+def test_environment_intersects ():
+
+    environment = Environment.Default()
+
+    assert environment.intersects() is not None
+
+################################################################################################################################################################
+
+def test_environment_accessObjects ():
+
+    environment = Environment.Default()
+
+    assert environment.accessObjects() is not None
+
+################################################################################################################################################################
+
+def test_environment_accessObjectWithName ():
+
+    environment = Environment.Default()
+
+    assert environment.accessObjectWithName('Earth') is not None
+
+################################################################################################################################################################
+
+def test_environment_accessCelestialObjectWithName ():
+
+    environment = Environment.Default()
+
+    assert environment.accessCelestialObjectWithName('Earth') is not None
+
+################################################################################################################################################################
+
+def test_environment_getInstant ():
+
+    environment = Environment.Default()
+
+    assert environment.getInstant() is not None
+
+################################################################################################################################################################
+
+def test_environment_getObjectNames ():
+
+    environment = Environment.Default()
+
+    assert environment.getObjectNames() is not None
+
+################################################################################################################################################################
+
+def test_environment_setInstant ():
+
+    environment = Environment.Default()
+
+    environment.setInstant(Instant.DateTime(DateTime(2019, 1, 1, 0, 0, 0), Scale.UTC))
 
 ################################################################################################################################################################
