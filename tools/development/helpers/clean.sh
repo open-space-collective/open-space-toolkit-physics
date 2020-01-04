932562596f86eca1565f4f492863426dec50120e
Lucas Brémond <lucas.bremond@gmail.com>
2020-01-04 10:51:10 -0800

    [misc] Fix script headers

--- tools/development/helpers/clean.sh
+++ tools/development/helpers/clean.sh
@@ -2,7 +2,7 @@
 
 ################################################################################################################################################################
 
-# @project        Library ▸ Mathematics
+# @project        Library ▸ Physics
 # @file           tools/development/helpers/clean.sh
 # @author         Lucas Brémond <lucas@loftorbital.com>
 # @license        Apache License 2.0
