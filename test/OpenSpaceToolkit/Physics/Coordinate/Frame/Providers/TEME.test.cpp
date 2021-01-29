2844da17215c2042732a1de98bb45201f1825870
robinpdm <robin@loftorbital.com>
2021-01-29 01:45:02 +0100

    fix: TEME/ITRF angular velocity

--- test/OpenSpaceToolkit/Physics/Coordinate/Frame/Providers/TEME.test.cpp
+++ test/OpenSpaceToolkit/Physics/Coordinate/Frame/Providers/TEME.test.cpp
@@ -16,6 +16,8 @@
 #include <OpenSpaceToolkit/Mathematics/Geometry/3D/Transformations/Rotations/Quaternion.hpp>
 #include <OpenSpaceToolkit/Mathematics/Objects/Vector.hpp>
 
+#include <OpenSpaceToolkit/Core/Types/Real.hpp>
+#include <OpenSpaceToolkit/Core/Containers/Triple.hpp>
 #include <OpenSpaceToolkit/Core/Containers/Table.hpp>
 #include <OpenSpaceToolkit/Core/Containers/Array.hpp>
 
@@ -26,12 +28,14 @@
 TEST (OpenSpaceToolkit_Physics_Coordinate_Frame_Providers_TEME, GetTransformAt)
 {
 
-    using ostk::core::ctnr::Pair ;
+    using ostk::core::types::Real ;
+    using ostk::core::ctnr::Triple ;
     using ostk::core::ctnr::Array ;
     using ostk::core::ctnr::Table ;
     using ostk::core::fs::Path ;
     using ostk::core::fs::File ;
 
+    using ostk::math::obj::Vector3d ;
     using ostk::math::geom::d3::trf::rot::Quaternion ;
     using ostk::math::geom::d3::trf::rot::RotationVector ;
 
@@ -45,10 +49,10 @@ TEST (OpenSpaceToolkit_Physics_Coordinate_Frame_Providers_TEME, GetTransformAt)
 
     {
 
-        const Array<Pair<File, Angle>> referenceScenarios =
+        const Array<Triple<File, Angle, Real>> referenceScenarios =
         {
-            { File::Path(Path::Parse("/app/test/OpenSpaceToolkit/Physics/Coordinate/Frame/Providers/TEME/Scenario_A/Scenario ITRF_TEME.csv")), Angle::Arcseconds(1.0) },
-            { File::Path(Path::Parse("/app/test/OpenSpaceToolkit/Physics/Coordinate/Frame/Providers/TEME/Scenario_B/Scenario ITRF_TEME.csv")), Angle::Arcseconds(20.0) }
+            { File::Path(Path::Parse("/app/test/OpenSpaceToolkit/Physics/Coordinate/Frame/Providers/TEME/Scenario_A/Scenario ITRF_TEME.csv")), Angle::Arcseconds(1.0), 1e-9 },
+            { File::Path(Path::Parse("/app/test/OpenSpaceToolkit/Physics/Coordinate/Frame/Providers/TEME/Scenario_B/Scenario ITRF_TEME.csv")), Angle::Arcseconds(20.0), 1e-9 }
         } ;
 
         for (const auto& referenceScenario : referenceScenarios)
@@ -70,7 +74,7 @@ TEST (OpenSpaceToolkit_Physics_Coordinate_Frame_Providers_TEME, GetTransformAt)
                 const Instant instant = Instant::DateTime(DateTime::Parse(referenceRow[0].accessString()), Scale::UTC) ;
 
                 const Quaternion referenceOrientation_ITRF_TEME = Quaternion::XYZS(referenceRow[1].accessReal(), referenceRow[2].accessReal(), referenceRow[3].accessReal(), referenceRow[4].accessReal()).normalize().conjugate() ;
-                const Quaternion referenceOrientation_GCRF_TEME = Quaternion::XYZS(referenceRow[5].accessReal(), referenceRow[6].accessReal(), referenceRow[7].accessReal(), referenceRow[8].accessReal()).normalize() ;
+                const Quaternion referenceOrientation_GCRF_TEME = Quaternion::XYZS(referenceRow[9].accessReal(), referenceRow[10].accessReal(), referenceRow[11].accessReal(), referenceRow[12].accessReal()).normalize() ;
 
                 const Transform transform_TEME_ITRF = temeFrameProvider.getTransformAt(instant) ;
 
@@ -91,7 +95,15 @@ TEST (OpenSpaceToolkit_Physics_Coordinate_Frame_Providers_TEME, GetTransformAt)
                 // << " : [TEME - GCRF]" << referenceOrientation_GCRF_TEME.toString(15) << " " << transform_TEME_GCRF.getOrientation().toString(15)
                 // << " >> " << RotationVector::Quaternion(dq_GCRF).getAngle().inArcseconds().toString(3) << " [asec]" << std::endl ;
 
-                // [TBI] Add angular velocity test
+                // Angular velocity test
+
+                const Vector3d referenceAngularVelocity_TEME_ITRF_in_TEME = { referenceRow[5].accessReal(), referenceRow[6].accessReal(), referenceRow[7].accessReal() } ;
+
+                const Vector3d w_TEME_ITRF_in_TEME = transform_TEME_ITRF.accessAngularVelocity() ;
+
+                const Real dw = referenceScenario.third ;
+
+                EXPECT_GT(dw, (w_TEME_ITRF_in_TEME - referenceAngularVelocity_TEME_ITRF_in_TEME).norm()) ;
 
             }
 
