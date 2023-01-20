5bbb4fe850cf81ba41ef43a113334c1981d9d5ca
Remy Derollez <43561754+Derollez@users.noreply.github.com>
2023-01-20 14:23:09 +0100

    fix: time interval intersects method binding (#92)
    * build: add tooling to mount dependencies repos
    
    * fix: intersection types and add args
    
    * refactor: unit tests on interval
    
    * Update tools/development/start.sh
    
    Co-authored-by: Robin Petitdemange <robin@loftorbital.com>
    
    Co-authored-by: Robin Petitdemange <robin@loftorbital.com>

--- tools/development/start.sh
+++ tools/development/start.sh
@@ -4,61 +4,108 @@
 
 # @project        Open Space Toolkit ▸ Physics
 # @file           tools/development/start.sh
-# @author         Lucas Brémond <lucas@loftorbital.com>
+# @author         Remy Derollez <remy@loftorbital.com>
 # @license        Apache License 2.0
 
 ################################################################################################################################################################
+
+# Check input arguments
 
 if [[ -z ${project_directory} ]]; then
     echo "Variable [project_directory] is undefined."
     exit 1
 fi
 
-options=""
-command="/bin/bash"
+if [[ -z ${docker_development_image_repository} ]]; then
+    echo "Variable [docker_development_image_repository] is undefined."
+    exit 1
+fi
+
+if [[ -z ${docker_image_version} ]]; then
+    echo "Variable [docker_image_version] is undefined."
+    exit 1
+fi
+
+if [[ -z ${target} ]]; then
+    echo "Variable [target] is undefined."
+    exit 1
+fi
+
+# Initialize variables
+
+options=()
+command=""
 
 # Setup linked mode
 
 if [[ ! -z ${1} ]] && [[ ${1} == "--link" ]]; then
 
-    options=""
-    command=""
+    for link in "${@:2}"
 
-    # Open Space Toolkit ▸ Core
+    do
 
-    if [[ -z ${open_space_toolkit_core_directory} ]]; then
-        echo "Variable [open_space_toolkit_core_directory] is undefined."
-        exit 1
-    fi
+        # Extract last part of the path
 
-    if [[ ! -d ${open_space_toolkit_core_directory} ]]; then
-        echo "Open Space Toolkit ▸ Core directory [${open_space_toolkit_core_directory}] cannot be found."
-        exit 1
-    fi
+        dep=${link##*/}
 
-    options="${options} \
-    --volume=${open_space_toolkit_core_directory}:/mnt/open-space-toolkit-core:ro"
+        # Log the linking step
 
-    command=" \
-    rm -rf /usr/local/include/OpenSpaceToolkit/Core; \
-    rm -f /usr/local/lib/libopen-space-toolkit-core.so*; \
-    cp -as /mnt/open-space-toolkit-core/include/OpenSpaceToolkit/Core /usr/local/include/OpenSpaceToolkit/Core; \
-    cp -as /mnt/open-space-toolkit-core/src/OpenSpaceToolkit/Core/* /usr/local/include/OpenSpaceToolkit/Core/; \
-    ln -s /mnt/open-space-toolkit-core/lib/libopen-space-toolkit-core.so /usr/local/lib/; \
-    ln -s /mnt/open-space-toolkit-core/lib/libopen-space-toolkit-core.so.0 /usr/local/lib/;"
+        echo "Linking with ${dep} at ${link}..."
 
-    # Open Space Toolkit ▸ I/O
+        # Open Space Toolkit ▸ Core
 
-    # TBI
+        if [[ ${dep} == "open-space-toolkit-core" ]]; then
 
-    # Open Space Toolkit ▸ Mathematics
+            options+=( "-v" )
+            options+=( "${link}:/mnt/open-space-toolkit-core:ro" )
 
-    # TBI
+            command="${command} \
+            rm -rf /usr/local/include/OpenSpaceToolkit/Core; \
+            rm -f /usr/local/lib/libopen-space-toolkit-core.so*; \
+            cp -as /mnt/open-space-toolkit-core/include/OpenSpaceToolkit/Core /usr/local/include/OpenSpaceToolkit/Core; \
+            cp -as /mnt/open-space-toolkit-core/src/OpenSpaceToolkit/Core/* /usr/local/include/OpenSpaceToolkit/Core/; \
+            ln -s /mnt/open-space-toolkit-core/lib/libopen-space-toolkit-core.so /usr/local/lib/; \
+            ln -s /mnt/open-space-toolkit-core/lib/libopen-space-toolkit-core.so.0 /usr/local/lib/;"
 
-    # Output
+        fi
 
-    command="${command} \
-    /bin/bash"
+        # Open Space Toolkit ▸ IO
+
+        if [[ ${dep} == "open-space-toolkit-io" ]]; then
+
+            options+=( "-v" )
+            options+=( "${link}:/mnt/open-space-toolkit-io:ro" )
+
+            command="${command} \
+            rm -rf /usr/local/include/OpenSpaceToolkit/IO; \
+            rm -f /usr/local/lib/libopen-space-toolkit-io.so*; \
+            cp -as /mnt/open-space-toolkit-io/include/OpenSpaceToolkit/IO /usr/local/include/OpenSpaceToolkit/IO; \
+            cp -as /mnt/open-space-toolkit-io/src/OpenSpaceToolkit/IO/* /usr/local/include/OpenSpaceToolkit/IO/; \
+            ln -s /mnt/open-space-toolkit-io/lib/libopen-space-toolkit-io.so /usr/local/lib/; \
+            ln -s /mnt/open-space-toolkit-io/lib/libopen-space-toolkit-io.so.0 /usr/local/lib/;"
+
+        fi
+
+        # Open Space Toolkit ▸ Mathematics
+
+        if [[ ${dep} == "open-space-toolkit-mathematics" ]]; then
+
+            options+=( "-v" )
+            options+=( "${link}:/mnt/open-space-toolkit-mathematics:ro" )
+
+            command="${command} \
+            rm -rf /usr/local/include/OpenSpaceToolkit/Mathematics; \
+            rm -f /usr/local/lib/libopen-space-toolkit-mathematics.so*; \
+            cp -as /mnt/open-space-toolkit-mathematics/include/OpenSpaceToolkit/Mathematics /usr/local/include/OpenSpaceToolkit/Mathematics; \
+            cp -as /mnt/open-space-toolkit-mathematics/src/OpenSpaceToolkit/Mathematics/* /usr/local/include/OpenSpaceToolkit/Mathematics/; \
+            ln -s /mnt/open-space-toolkit-mathematics/lib/libopen-space-toolkit-mathematics.so /usr/local/lib/; \
+            ln -s /mnt/open-space-toolkit-mathematics/lib/libopen-space-toolkit-mathematics.so.0 /usr/local/lib/;"
+
+        fi
+
+    done
+
+    command="${command} /bin/bash"
 
 fi
 
@@ -68,9 +115,9 @@ docker run \
 -it \
 --rm \
 --privileged \
-${options} \
---volume="${project_directory}:/app:delegated" \
---volume="${project_directory}/tools/development/helpers:/app/build/helpers:ro,delegated" \
+"${options[@]}" \
+-v "${project_directory}:/app:delegated" \
+-v "${project_directory}/tools/development/helpers:/app/build/helpers:ro,delegated" \
 --workdir="/app/build" \
 ${docker_development_image_repository}:${docker_image_version}-${target} \
 /bin/bash -c "${command}"
