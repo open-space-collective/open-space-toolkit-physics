4db4d1cdc20061f632fedcca4da1c67568548928
Vishwa Shah <vishwa@users-MacBook-Pro.local>
2024-02-16 20:51:36 -0800

    chore: add tests for Scalar, Vector and Direction

---
+++ test/OpenSpaceToolkit/Physics/Data/Direction.test.cpp
@@ -0,0 +1,57 @@
+/// Apache License 2.0
+
+#include <OpenSpaceToolkit/Physics/Data/Direction.hpp>
+
+#include <Global.test.hpp>
+
+using ostk::core::type::Real;
+using ostk::core::type::Shared;
+
+using ostk::mathematics::object::Vector3d;
+
+using ostk::physics::data::Direction;
+using ostk::physics::coordinate::Frame;
+
+class OpenSpaceToolkit_Physics_Data_Direction : public ::testing::Test
+{
+   protected:
+    const Vector3d value_ = {1.0, 2.0, 3.0};
+    const Shared<const Frame> frameSPtr_ = Frame::GCRF();
+    const Direction direction_ = {
+        value_,
+        frameSPtr_,
+    };
+};
+
+TEST_F(OpenSpaceToolkit_Physics_Data_Direction, EqualToOperator)
+{
+    {
+        EXPECT_TRUE(direction_ == direction_);
+    }
+}
+
+TEST_F(OpenSpaceToolkit_Physics_Data_Direction, NotEqualToOperator)
+{
+    {
+        EXPECT_FALSE(direction_ != direction_);
+    }
+}
+
+TEST_F(OpenSpaceToolkit_Physics_Data_Direction, StreamOperator)
+{
+    {
+        testing::internal::CaptureStdout();
+
+        EXPECT_NO_THROW(std::cout << direction_ << std::endl);
+
+        EXPECT_FALSE(testing::internal::GetCapturedStdout().empty());
+    }
+}
+
+TEST_F(OpenSpaceToolkit_Physics_Data_Direction, Undefined)
+{
+    {
+        EXPECT_FALSE(Direction::Undefined().isDefined());
+    }
+}
+
---
+++ test/OpenSpaceToolkit/Physics/Data/Scalar.test.cpp
@@ -0,0 +1,91 @@
+/// Apache License 2.0
+
+#include <OpenSpaceToolkit/Physics/Data/Scalar.hpp>
+
+#include <Global.test.hpp>
+
+using ostk::core::type::Real;
+
+using ostk::physics::data::Scalar;
+using ostk::physics::Unit;
+using ostk::physics::unit::Length;
+
+class OpenSpaceToolkit_Physics_Data_Scalar : public ::testing::Test
+{
+   protected:
+    const Unit unit_ = Unit::Length(Length::Unit::Meter);
+    const Real value_ = 5.0;
+    const Scalar scalar_ = {value_, unit_};
+};
+
+TEST_F(OpenSpaceToolkit_Physics_Data_Scalar, EqualToOperator)
+{
+    {
+        EXPECT_TRUE(scalar_ == scalar_);
+    }
+}
+
+TEST_F(OpenSpaceToolkit_Physics_Data_Scalar, NotEqualToOperator)
+{
+    {
+        EXPECT_FALSE(scalar_ != scalar_);
+    }
+}
+
+TEST_F(OpenSpaceToolkit_Physics_Data_Scalar, StreamOperator)
+{
+    {
+        testing::internal::CaptureStdout();
+
+        EXPECT_NO_THROW(std::cout << scalar_ << std::endl);
+
+        EXPECT_FALSE(testing::internal::GetCapturedStdout().empty());
+    }
+}
+
+TEST_F(OpenSpaceToolkit_Physics_Data_Scalar, IsDefined)
+{
+    {
+        EXPECT_FALSE(Scalar::Undefined().isDefined());
+    }
+
+    {
+        EXPECT_TRUE(scalar_.isDefined());
+    }
+}
+
+TEST_F(OpenSpaceToolkit_Physics_Data_Scalar, GetValue)
+{
+    {
+        EXPECT_EQ(scalar_.getValue(), value_);
+    }
+}
+
+TEST_F(OpenSpaceToolkit_Physics_Data_Scalar, GetUnit)
+{
+    {
+        EXPECT_EQ(scalar_.getUnit(), unit_);
+    }
+}
+
+TEST_F(OpenSpaceToolkit_Physics_Data_Scalar, InUnit)
+{
+    {
+        EXPECT_TRUE(scalar_.inUnit(Unit::Length(Length::Unit::Foot)).isDefined());
+    }
+}
+
+TEST_F(OpenSpaceToolkit_Physics_Data_Scalar, ToString)
+{
+    {
+        EXPECT_EQ(scalar_.toString(), "5.0 [m]");
+    }
+}
+
+TEST_F(OpenSpaceToolkit_Physics_Data_Scalar, Undefined)
+{
+    {
+        EXPECT_FALSE(Scalar::Undefined().isDefined());
+    }
+}
+
