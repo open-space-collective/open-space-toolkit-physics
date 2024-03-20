3f8c52c5dab283b63c67e6086f6b88aa7f1a27f0
Kyle Cochran <kyle.cochran@loftorbital.com>
2024-03-20 17:34:28 +0000

    fix: more manager test problesm

--- bindings/python/test/data/test_manager.py
+++ bindings/python/test/data/test_manager.py
@@ -2,16 +2,19 @@
 
 import pytest
 from datetime import datetime
-from ostk.core.filesystem import Directory, File
+from ostk.core.filesystem import Directory, File, Path
+
+from ostk.io import URL
+
 from ostk.physics.time import Instant
 from ostk.physics.data import Manager, Manifest
 
 class TestManager:
     def test_get_last_update_timestamp_for_success(self, manager: Manager):
-        assert isinstance(manager.get_last_update_timestamp_for("manifest"), datetime)
+        assert isinstance(manager.get_last_update_timestamp_for("manifest"), Instant)
 
     def test_get_remote_url_success(self, manager: Manager):
-        assert isinstance(manager.get_remote_url(), str)
+        assert isinstance(manager.get_remote_url(), URL)
 
     def test_get_manifest_repository_success(self, manager: Manager):
         assert isinstance(manager.get_manifest_repository(), Directory)
@@ -20,39 +23,41 @@ class TestManager:
         assert isinstance(manager.get_remote_data_urls("data_name"), list)
 
     def test_get_manifest_success(self, manager: Manager):
-        assert isinstance(manager.get_manifest(), dict)
+        assert isinstance(manager.get_manifest(), Manifest)
 
     def test_set_remote_url_success(self, manager: Manager):
-        manager.set_remote_url("http://example.com")
-        assert manager.get_remote_url() == "http://example.com"
+        manager.set_remote_url(URL.parse("http://example.com"))
+        assert manager.get_remote_url() == URL.parse("http://example.com")
 
     def test_set_manifest_repository_success(self, manager: Manager):
-        directory = Directory.path("./test_directory")
+        directory = Directory.path(Path.parse("./test_directory"))
         manager.set_manifest_repository(directory)
         assert manager.get_manifest_repository() == directory
 
     def test_find_remote_data_urls_success(self, manager: Manager):
         assert isinstance(manager.find_remote_data_urls(".*"), list)
 
-    def test_load_manifest_success(self, manager: Manager, manifest_file: File):
-
-        manifest = Manifest.load(manifest_file)
+    def test_load_manifest_success(self, manager: Manager, manifest: Manifest):
 
         manager.load_manifest(manifest)
-        assert manager.get_manifest() == manifest
+        assert manager.get_manifest().is_defined()
 
     def test_manifest_file_exists_success(self, manager: Manager):
         assert isinstance(manager.manifest_file_exists(), bool)
 
-    def test_reset_success(self, manager: Manager):
+    def test_reset_success(self, manager: Manager, manifest: Manifest):
+
+        manager.load_manifest(manifest)
+        assert manager.get_manifest().is_defined()
+
         manager.reset()
-        assert manager.get_manifest() == {}
+        assert not manager.get_manifest().is_defined()
 
     def test_get_success(self):
         assert isinstance(Manager.get(), Manager)
 
     def test_default_remote_url_success(self, manager: Manager):
-        assert isinstance(manager.default_remote_url(), str)
+        assert isinstance(manager.default_remote_url(), URL)
 
     def test_default_manifest_repository_success(self, manager: Manager):
         assert isinstance(manager.default_manifest_repository(), Directory)
